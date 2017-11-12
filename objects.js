bb83e943041fe5e7700e459884db0660a85dca34
Luke Conley <luke.sovern.conley@gmail.com>
2017-11-12 18:19:33 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -10,7 +10,7 @@ function updatePlaylist(playlist, artist, song) {
 }
 
 function removeFromPlaylist(playlist, artist) {
-  delete playlist.artist;
+  delete playlist[artist];
 
   return playlist;
 }
